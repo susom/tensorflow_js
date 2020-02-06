08315b916d653dd5896aaaa4c38bb291156296e8
Andrew Martin <andy123@stanford.edu>
2020-02-06 11:29:35 -0800

    adding save method

--- REDCapJsRenderer.php
+++ REDCapJsRenderer.php
@@ -14,27 +14,124 @@ use Sabre\DAV\Exception;
 
 class REDCapJsRenderer
 {
+
     /**
-     * Return the 'metadata' for rendering the requested instrument
-     *
-     * @param      $project_id
-     * @param      $event_id
-     * @param      $form_name
-     * @param null $record_id
-     * @param null $instance_id
+     * When a field is modified on the client, an ajax call will post the data here
+     * The format of the save should be:
+     * [
+     *  "hash" => "asdfasdf",
+     *  "fields" => [
+     *      "field_name" => "value",
+     *      "field_name" => "value2",
+     *      ...
+     *  ]
+     * @param $data
+     * @return mixed
+     * @throws Exception
      */
-    static function getForm($project_id, $event_id, $form_name, $record_id = null, $instance_id = null) {
+    static function saveData($data) {
+        global $module;
+
+        if (empty($data['hash'])) {
+            throw new Exception ('Missing hash');
+        }
+        $hash = $data['hash'];
+
+        if (empty($data['fields'])) {
+            throw new Exception ('Missing fields to save');
+        }
+        $fields = $data['fields'];
+
+        // If the hash is for a 'new record' then we need to create a record first
+        $params = self::lookupHash($hash);
+        if (empty($params)) {
+            throw new Exception ('Supplied hash $hash is invalid');
+        }
+
+        $log_id     = $params['log_id'];
+        $project_id = $params['project_id'];
+        $event_id   = $params['event_id'];
+        $form_name  = $params['form_name'];
+        $record     = $params['record'];
+        $instance   = $params['instance'];
+        $timestamp  = $params['timestamp'];
+
+        //TODO: Add support for instances
+
+        if (empty($record)) {
+            // This was a 'public' hash so we have to create a record-specific hash now
+            $nextRecord = getautoid($project_id, false);
+            $newHash = self::createHash($project_id, $event_id, $form_name, $nextRecord, $instance);
+            $module->emDebug("Creating new record: $nextRecord with new hash $newHash");
+        }
+
+        // Do we save all at once or one at a time?  Presumably all at once for speed?
+        $fields = [];
+        $valid_fields = self::getValidFields($project_id,$event_id,$form_name);
+        $saveFields = [];
+        foreach ($fields as $field_name => $field_values) {
+
+            // Make sure the field is valid
+            if (!in_array($field_name, $valid_fields)) {
+                $module->emError("Attempt to save invalid field $field_name", $field_values, $valid_fields);
+                continue;
+            }
+
+            // TODO: Discuss how to save checkbox values...  Currently client must post ALL boxes in each change
+            $saveFields[$field_name] = $field_values;
+        }
+
+        $saveData = array(
+            $record => array(
+                $event_id => $saveFields
+            )
+        );
+
+        $result = \REDCap::saveData($project_id,'array', $saveData, 'overwrite');
+
+        $module->emDebug("Save Result", $result, $saveData);
+
+        return $result;
+        // $hash = isset($data['hash']@$data['hash'];
 
     }
 
 
-    static function saveHash($hash, $minutes_to_expire = 60) {
+    /**
+     * Utility to return array of valid metadata as an indexed array
+     * [0] => record_id,
+     * [1] => field_one, ...
+     *
+     * @param $project_id
+     * @param $event_id
+     * @param $form_name
+     * @return array
+     * @throws \Exception
+     */
+    private static function getValidFields($project_id, $event_id, $form_name) {
+        // Get the project object
+        global $Proj;
+        $_Proj = $Proj->project_id == $project_id ? $Proj : new \Project($project_id);
 
+        // Let's assemble an array of valid fields for this form, starting with event_id:
+        $valid_fields = \REDCap::getValidFieldsByEvents($project_id, $event_id);
+
+        // If we have a form, then let's also filter fields by form
+        if (!empty($form_name)) {
+            $form_fields = \REDCap::getFieldNames($form_name);
+            $valid_fields = array_unique(array_merge($valid_fields, $form_fields));
+        }
+
+        return $valid_fields;
     }
+
+
 
     /**
      * Take in the hash and then determine what metadata should be returned
+     * If there is an existing record, let's include that data as well
      * @param $hash
+     * @return array|mixed
      * @throws Exception
      */
     static function getMetadata($hash) {
@@ -93,43 +190,43 @@ class REDCapJsRenderer
                 }
             }
 
-            // Let's assemble an array of valid fields for this form, starting with event_id:
-            $valid_fields = \REDCap::getValidFieldsByEvents($project_id, $event_id);
-
-            // If we have a form, then let's also filter fields by form
-            if (!empty($form_name)) {
-                $form_fields = \REDCap::getFieldNames($form_name);
-                $valid_fields = array_unique(array_merge($valid_fields, $form_fields));
-            }
+            // Get the valid fields for this event/form combination
+            $valid_fields = self::getValidFields($project_id, $event_id, $form_name);
 
             // Let's filter out only those fields that are in the valid_fields array above from all Metadata
             $valid_metadata = array_intersect_key($_Proj->metadata, array_flip($valid_fields));
 
             // $module->emDebug(count($metadata_all), count($valid_fields), count($metadata));
-            // $module->emDebug($valid_fields);
+            $module->emDebug($valid_fields);
 
-            // If we have a record/instance, we need to include the data with this metadata
+            // If we have a record/instance, we need to include the current record's data with this metadata
             if (!empty($record)) {
                 // TODO: Implement ability to handle instance numbers - punting for now!
 
                 // Get Data
-                $result = \REDCap::getData($project_id, 'array', $record, $valid_fields, $event_id);
+                $q = \REDCap::getData($project_id, 'array', $record, $valid_fields, $event_id);
 
-                foreach ($result[$record][$event_id] as $field_name => $field_data) {
-                    $valid_metadata[$field_name]['current_value'] = $field_data;
+                if (empty($result[$record][$event_id])) {
+                    $module->emError("Unable to find record $record, event $event_id in query results", $q);
+                } else {
+
+                    // Lets add data to the valid metadata
+                    foreach ($q[$record][$event_id] as $field_name => $field_data) {
+                        if (isset($valid_metadata[$field_name])) {
+                            $valid_metadata[$field_name]['current_value'] = $field_data;
+                        } else {
+                            $module->emError("Data returned field $field_name which isn't part of the valid_metadata", $valid_metadata);
+                        }
+                    }
                 }
             }
 
-            $module->emDebug($valid_metadata);
-
-
+            // $module->emDebug($valid_metadata);
+            $result = $valid_metadata;
         }
 
-
-
+        return $result;
     }
-
-
 
 
     /**
@@ -221,7 +318,7 @@ class REDCapJsRenderer
      * @param $project_id
      * @return string
      */
-    static function getUniqueHash($project_id) {
+    private static function getUniqueHash($project_id) {
         global $module;
 
         do{
